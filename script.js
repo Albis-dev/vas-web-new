58f832180b2bd6b273a46eaacee8e8e3c52d929c
Albis <joosahoon@gmail.com>
2020-02-10 23:59:31 +0900

    카카오지도 API 적용

--- script.js
+++ script.js
@@ -22,6 +22,28 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+// KAKAOMAP
+
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+  center: new kakao.maps.LatLng(37.536293, 126.943946), //지도의 중심좌표.
+  draggable: false,
+	level: 3 //지도의 레벨(확대, 축소 정도)
+};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(37.536293, 126.943946); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
 // MOBILE NAVBAR BURGER
 const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 if (vw<=1024) {
@@ -34,24 +56,3 @@ if (vw<=1024) {
 window.onload = function() {
 	scrolly();
 }; 
-
-var btn0 = document.querySelector("#popmodal0")
-var mdl0 = new BulmaModal("#modal0")
-
-var btn1 = document.querySelector("#popmodal1")
-var mdl1 = new BulmaModal("#modal1")
-
-var btn2 = document.querySelector("#popmodal2")
-var mdl2 = new BulmaModal("#modal2")
-
-btn0.addEventListener("click", function () {
-	mdl0.show()
-})
-
-btn1.addEventListener("click", function () {
-	mdl1.show()
-})
-
-btn2.addEventListener("click", function () {
-	mdl2.show()
-})
